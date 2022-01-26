1702e5a5bb96a2c6d3cbd8bda8dc9e3b41e24cbf
Kristof Poszet <kristof_poszet@epam.com>
2022-01-26 16:53:26 +0100

    class level constants

--- src/models/Car.js
+++ src/models/Car.js
@@ -3,11 +3,15 @@ export class Car {
     this.manufacturer = manufacturer;
     this.model = model;
     this.year = year;
-    //is car classic or not?
     this.isClassic = isClassic;
   }
 
   toString() {
-    return `${this.manufacturer} ${this.model} [${this.year}] ${this.isClassic ? 'C' : 'S'}`;
+    return `${this.manufacturer} ${this.model} [${this.year}] ${
+      this.isClassic ? Car.CLASSIC_CAR_ABBREVIATION : Car.SIMPLE_CAR_ABBREVIATION
+    }`;
   }
 }
+
+Car.CLASSIC_CAR_ABBREVIATION = 'C';
+Car.SIMPLE_CAR_ABBREVIATION = 'S';
