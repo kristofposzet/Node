d5d638965a3c6b922f943d58d496735e4ceaf6e3
Kristof Poszet <kristof_poszet@epam.com>
2022-02-01 16:51:04 +0100

    first http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 require('dotenv').config();
+const http = require('http');
+const PORT = process.env.PORT || 8080,
+  green = (text) => `\x1b[32m${text}\x1b[0m 🎉`,
+  server = http.createServer();
+
+server.on('request', (req, res) => {
+  const { url } = req;
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = 200;
+  res.end(JSON.stringify(url));
+});
+
+server.listen(PORT, () => process.stdout.write(`Server listening on port: ${green(PORT)}\n`));
